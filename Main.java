14765ba9e36f767aebbfb290eb6eb45c63321e3f
Rxcthefirst <81718883+Rxcthefirst@users.noreply.github.com>
2022-10-08 17:57:31 -0700

    First commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+		IDandPasswords idandPasswords = new IDandPasswords();
+		
+		LoginPage loginPage = new LoginPage(idandPasswords.getLoginInfo());
+	}
+
+}
